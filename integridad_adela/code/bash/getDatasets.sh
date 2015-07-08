b2d8b32d9abaa110e6e258478a41f6cd02ac6c1f
lromang <luis.roangarci@gmail.com>
2015-07-08 16:09:59 -0500

    consulta api

--- integridad_adela/code/bash/getDatasets.sh
+++ integridad_adela/code/bash/getDatasets.sh
@@ -1,4 +1,9 @@
 #! /usr/bin/bash
+./getCatalog.sh
+./getDependencies.sh
+for i in $(cat ../../data/dependencies.txt)
+do
+    curl -ls $i | jq -c '{catalogo:.["title"],modified:.["modified"],base:.dataset[]["distribution"][]["title"],byteSize:.dataset[]["distribution"][]["byteSize"],mediaType:.dataset[]["distribution"][]["mediaType"]}'
+done
 
- for i in $(cat ../../data/dependencies.txt); do curl -sL "$i" | jq '.' >> ../../data/datasets/data$i ; done
 
--- integridad_adela/code/bash/getDependencies.sh
+++ integridad_adela/code/bash/getDependencies.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/bash
 
-for i in $(cat ../../data/catalogs.json | sed 's;/; ;g' | awk '{print $3}' ); do echo "adela.datos.gob.mx/$i/catalogo.json" >> ../../data/dependencies.txt ;done
+for i in $(cat ../../data/catalogs.json | awk -F '/' '{print $4}' ); do echo "adela.datos.gob.mx/$i/catalogo.json" >> ../../data/dependencies.txt ;done
 
