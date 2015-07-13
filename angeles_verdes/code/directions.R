9e820916448b4be52f6eb8e69c0bb6b4643b7d39
lromang <luis.roangarci@gmail.com>
2015-07-13 17:38:19 -0500

    correción directions

--- angeles_verdes/code/directions.R
+++ angeles_verdes/code/directions.R
@@ -17,7 +17,7 @@ get_directions <- function(origen, destino, mode = "driving"){
     key         <- "key=AIzaSyAkW2m1J6oq_UblEtwhzVB9EYmz7Ayc4k0"
     query       <- paste(base,origen,destino,mode,key,sep = "&")
     route       <- fromJSON(getURL(query))$routes[[1]]$legs
-    steps       <- routes[[1]]$steps
+    steps       <- route[[1]]$steps
     list(
         "durations"  = ldply(steps,function(t)t <- t$duration$text)[,1],
         "distance"   = ldply(steps,function(t)t <- t$distance$text)[,1],
