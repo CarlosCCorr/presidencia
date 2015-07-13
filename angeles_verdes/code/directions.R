a53467e651d4ce0c870c6db1da555f78ba31dc7e
lromang <luis.roangarci@gmail.com>
2015-07-13 18:52:56 -0500

    distancias ángeles verdes

--- angeles_verdes/code/directions.R
+++ angeles_verdes/code/directions.R
@@ -3,6 +3,8 @@ library(rjson)
 library(RJSONIO)
 library(RCurl)
 library(plyr)
+library(stringr)
+library(data.table)
 ## Crear query
 origen <- "DF"
 destino <- "Guadalajara"
@@ -40,3 +42,36 @@ get_distance <- function(origen, destino){
     list("distance"=distance,"duration"=duration)
 }
 distance <- get_distance(origen,destino)
+## Determina los puntos que se encuentran a
+## una distancia medida en norma infinito
+## menor a cierto parámetro
+inf_dist <- function(coords, array, dist){
+    array.long.near <- array[
+                             abs(array$long -
+                                     coords[2]) <
+                                 dist, ]
+    array.lat.near  <- array.long.near[
+                                       abs(array.long.near$lat -
+                                               coords[1]) <
+                                           dist, ]
+    array.lat.near
+}
+## Determina los vehículos de rescate
+## que se encontraban a una distancia mínima del
+## punto del accidente.
+get_min_resc  <- function(accident, rescuers){
+    distances <- ldply(rescuers, function(t)t <- get_distance(t,accident)$distance)[,1]
+    distances[which(distances)==min(distances)]
+}
+############################ Pruebas ############################
+## lectura de datos
+## Recorridos de patrullas
+recorridos <- read.csv("../angeles_verdes/data/recorridos.csv", sep = ",")
+setkey(recorridos,Patrulla)
+setnames(recorridos, tolower(names(recorridos)))
+## Accidentes
+accidentes <- fread("../angeles_verdes/data/angels_clean.csv", sep = ",")
+setnames(accidentes, tolower(names(accidentes)))
+############################ Análisis ############################
+## Análisis exploratorio
+test_coord <- c(30.637415,-112.541097)
