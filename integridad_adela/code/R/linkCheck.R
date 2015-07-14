46eec673d45976dd860a3c0f3c7d8638ab556654
lromang <luis.roangarci@gmail.com>
2015-07-14 18:51:07 -0500

    sampling linkChecker

--- integridad_adela/code/R/linkCheck.R
+++ integridad_adela/code/R/linkCheck.R
@@ -4,19 +4,15 @@
 library(httr)
 library(plyr)
 ## Lectura de datos (muestras de 15)
-data   <- read.csv( "/home/lgarcia/proyectos/presidencia/data_analysis/serverUp/code/R/MAT.csv", stringsAsFactors = FALSE )
-data   <- data[ data$Version == 1, ]
+data   <- read.csv( "../../data/MAT.csv", stringsAsFactors = FALSE )
+sample <- sample(nrow(data),100)
 url    <- data$URL
+samp   <- url[sample]
 ## Prueba
-ldply(url,
-                function( t ){ u <-
+ldply(samp, function( t ){ u <-
                     http_status( GET( t ) )
-                               if(u$category != "success"){
-                                   print(which(url==t))
-                               }
-                               u$category } )
-## result <- data.frame( url = data$URL, test_result = test[,1] )
-## data$result <- test[,1]
-## write.csv(data, "./MAT_RES.csv", row.names = FALSE)
-## Imprimir resultados
-## print(result)
+                    if(u$category != "success"){
+                    	print(which(url==t)[1])
+                    }
+                } )
+
